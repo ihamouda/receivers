47276ea32c3d84d89c5a46c4819674245802f268
Ibrahim Hamouda <ihamouda@procuredox.com>
2017-06-21 04:35:05 -0600

    Fixed ResourceConfig

--- src/main/java/com/procuredox/receivers/ApiApplication.java
+++ src/main/java/com/procuredox/receivers/ApiApplication.java
@@ -1,10 +1,13 @@
 package com.procuredox.receivers;
 
+import org.glassfish.jersey.server.ResourceConfig;
+
 import javax.ws.rs.ApplicationPath;
 
 /**
  * Created by ihamouda on 2017-06-21.
  */
 @ApplicationPath("/")
-public class ApiApplication {
+public class ApiApplication extends ResourceConfig {
+    public ApiApplication(){register(ApiResources.class);}
 }
