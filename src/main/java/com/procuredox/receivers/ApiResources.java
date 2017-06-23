500997854e5278fc2df29496548f0e2ce879334c
Ibrahim Hamouda <ihamouda@procuredox.com>
2017-06-23 05:33:12 -0600

    Added CAT service

--- src/main/java/com/procuredox/receivers/ApiResources.java
+++ src/main/java/com/procuredox/receivers/ApiResources.java
@@ -1,11 +1,15 @@
 package com.procuredox.receivers;
 
 import org.apache.commons.io.FileUtils;
+import org.glassfish.jersey.internal.util.Base64;
+import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 import java.io.File;
 import java.io.IOException;
@@ -51,6 +55,24 @@ public class ApiResources {
         }catch (Exception e){
             e.printStackTrace();
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+        }
+    }
+
+    @POST
+    @Path("/cat")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response getCatPo(@FormParam("CXML-base64") String cxml){
+        try {
+            String decoded = Base64.decode(cxml.getBytes()).toString();
+            FileUtils.writeByteArrayToFile(
+                    new File("c:/Users/ihamouda/Documents/CAT/tests/cat_order.xml"), decoded.toString().getBytes());
+            return Response.status(Response.Status.OK).build();
+        }catch (IOException e){
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }catch (Exception e){
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
     }
 }
