cbae8e402c08627d42cfa2f6dd8b0c1e811d56b8
m.krestovskiy <m.krestovsky@thprom.ru>
2017-09-02 01:41:24 +0300

    change status to dropped to vendor

--- src/main/java/com/procuredox/receivers/ResendPOResponse.java
+++ src/main/java/com/procuredox/receivers/ResendPOResponse.java
@@ -25,4 +25,20 @@ public class ResendPOResponse {
     public void setSuccess(boolean success) {
         this.success = success;
     }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getStackTrace() {
+        return stackTrace;
+    }
+
+    public void setStackTrace(String stackTrace) {
+        this.stackTrace = stackTrace;
+    }
 }
